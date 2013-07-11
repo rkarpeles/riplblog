e2c53e1c27f76d8a8fa84c8d13c4bf6533ccc2da
Ryan Karpeles <rkarpel@jmls.edu>
2013-07-11 14:06:26 -0500

    Stylesheet link to ripl.css

--- wp-content/themes/starkers-master/parts/shared/html-header.php
+++ wp-content/themes/starkers-master/parts/shared/html-header.php
@@ -12,8 +12,12 @@
 	  	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+		<!--Custom stylesheet for this particular JMLS blog-->
+		<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/css/ripl.css"/>
+		<!--Favicon-->
 		<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon.ico"/>	
-        <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/picturefill.min.js"></script> 		
+		<!--Picturefill for responsive images: https://github.com/scottjehl/picturefill-->
+		<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/picturefill.min.js"></script> 		
 		<!--[if IE]>
 		    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/css/ie.css">
 			<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/html5shiv.js"></script> 
