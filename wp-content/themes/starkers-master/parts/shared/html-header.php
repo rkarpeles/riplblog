fb16d43e2fae1afa566023aef10e509062f7174d
Ryan Karpeles <rkarpel@jmls.edu>
2013-07-16 13:19:47 -0500

    Moved css above js in header. Removed @import for reset.css

--- wp-content/themes/starkers-master/parts/shared/html-header.php
+++ wp-content/themes/starkers-master/parts/shared/html-header.php
@@ -6,16 +6,19 @@
 <!--[if IE 9 ]><html class="no-js ie9 lt-ie10" lang="en"><![endif]--> 
 <!--[if (gte IE 9)|(gt IEMobile 7)|!(IEMobile)|!(IE)]><!--><html class="no-js" lang="en"><!--<![endif]-->
 	<head>
-		<title><?php bloginfo( 'name' ); ?><?php wp_title( '|' ); ?></title>
-		<?php wp_enqueue_script("jquery"); ?>
+		<title><?php bloginfo( 'name' ); ?><?php wp_title( '|' ); ?></title>		
 		<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	  	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+		<!--Reset stylesheet-->
+		<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/css/reset.css"/>
 		<!--Custom stylesheet for this particular JMLS blog-->
 		<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/css/ripl.css"/>
 		<!--Favicon-->
-		<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon.ico"/>	
+		<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon.ico"/>
+		<!--Load jQuery script-->
+		<?php wp_enqueue_script("jquery"); ?>
 		<!--Picturefill for responsive images: https://github.com/scottjehl/picturefill-->
 		<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/picturefill.min.js"></script> 		
 		<!--[if IE]>
