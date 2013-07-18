92e4eb2fb78c9abb702401f476e8fa05788dcc10
Ryan Karpeles <rkarpel@jmls.edu>
2013-07-18 14:45:48 -0500

    Move jQuery call in header

--- wp-content/themes/starkers-master-child/parts/shared/html-header.php
+++ wp-content/themes/starkers-master-child/parts/shared/html-header.php
@@ -8,15 +8,15 @@
 	<head>
 		<title><?php bloginfo( 'name' ); ?><?php wp_title( '|' ); ?></title>		
 		<meta charset="<?php bloginfo( 'charset' ); ?>" />
+		<!--Load jQuery script-->
+		<?php wp_enqueue_script("jquery"); ?>
 	  	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />		
 		<!--Custom stylesheet for this particular JMLS blog-->
 		<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/css/ripl.css"/>
 		<!--Favicon-->
-		<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/images/favicon.ico"/>
-		<!--Load jQuery script-->
-		<?php wp_enqueue_script("jquery"); ?>
+		<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/images/favicon.ico"/>		
 		<!--Picturefill for responsive images: https://github.com/scottjehl/picturefill-->
 		<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/picturefill.min.js"></script> 		
 		<!--[if IE]>
