c6a743babc410dec03e63de9043623fdea53f7f2
Ryan Karpeles <rkarpel@jmls.edu>
2013-07-18 14:51:49 -0500

    Moving stuff

--- wp-content/themes/starkers-master-child/parts/shared/html-header.php
+++ wp-content/themes/starkers-master-child/parts/shared/html-header.php
@@ -7,16 +7,14 @@
 <!--[if (gte IE 9)|(gt IEMobile 7)|!(IEMobile)|!(IE)]><!--><html class="no-js" lang="en"><!--<![endif]-->
 	<head>
 		<title><?php bloginfo( 'name' ); ?><?php wp_title( '|' ); ?></title>		
-		<meta charset="<?php bloginfo( 'charset' ); ?>" />
-		<!--Load jQuery script-->
-		<?php wp_enqueue_script("jquery"); ?>
+		<meta charset="<?php bloginfo( 'charset' ); ?>" />		
 	  	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />		
-		<!--Custom stylesheet for this particular JMLS blog-->
-		<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/css/ripl.css"/>
 		<!--Favicon-->
-		<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/images/favicon.ico"/>		
+		<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/images/favicon.ico"/>	
+        <!--Load jQuery script-->
+		<?php wp_enqueue_script("jquery"); ?>		
 		<!--Picturefill for responsive images: https://github.com/scottjehl/picturefill-->
 		<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/picturefill.min.js"></script> 		
 		<!--[if IE]>
